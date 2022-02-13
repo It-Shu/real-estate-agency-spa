cf4d79ec9c56913d86af946473fd1c81968a69e4
It-Shu <it.shustov@gmail.com>
2022-02-13 23:14:09 +0300

    refactor

--- src/Routs/routes.ts
+++ src/Routs/routes.ts
@@ -1,9 +1,5 @@
-// import {FC} from 'react'
-// import Contacts from '../Pages/Contacts/Contacts';
-// import Home from "../Pages/Home/Home";
-// import News from '../Pages/News/News';
-// import Objects from "../Pages/Objects/Objects";
-// import Header from "../Pages/Header/Header";
+
+
 
 export enum PATH {
     HOME = '/',
@@ -13,16 +9,3 @@ export enum PATH {
     CONTACTS = '/contacts',
     OBJECTS = '/objects',
 }
-
-// type Routes = {
-//     path: string,
-//     element: FC
-//     exact?: boolean
-// }
-
-// export const publicRoutes: Routes[] = [
-//     {path: PATH.HOME, element: Home, exact: true},
-//     {path: PATH.NEWS, element: News},
-//     {path: PATH.CONTACTS, element: Contacts},
-//     {path: PATH.OBJECTS, element: Objects},
-// ]
